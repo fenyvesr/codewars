74008e67939a664d81386145173a999b8f741bb7
fro2bp <Robert.Fenyvesi@hu.bosch.com>
2021-12-08 12:28:11 +0100

    Initializing doxygen     - Create doxygen config     - Add documentation generation to build     - Add doxygen as a Tool

--- C++/build.bat
+++ C++/build.bat
@@ -1,4 +1,6 @@
 
 CALL Tools\cmake-3.22.1-windows-x86_64\bin\cmake.exe -S ./ -B ./build
 CALL Tools\cmake-3.22.1-windows-x86_64\bin\cmake.exe --build ./build
+CALL Tools\doxygen-1.9\doxygen.exe Doxygen_config
+
 CALL build\Debug\Codewars.exe
