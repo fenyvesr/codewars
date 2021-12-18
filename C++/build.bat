24e3dca790518878897a50ff83a26f30785a1e55
fenyvesr <fenyvesr@gmail.com>
2021-12-18 11:20:22 +0100

    Update tools

--- C++/build.bat
+++ C++/build.bat
@@ -4,6 +4,6 @@
 CALL Tools\cmake-3.22.1-windows-x86_64\bin\cmake.exe -B ./build -DCMAKE_BUILD_TYPE=Release
 CALL Tools\cmake-3.22.1-windows-x86_64\bin\cmake.exe --build ./build --config Release
 CALL Tools\cmake-3.22.1-windows-x86_64\bin\ctest.exe -C Release --output-on-failure
-CALL Tools\doxygen-1.9\doxygen.exe Doxygen_config
+CALL Tools\doxygen-1.9.2.windows.x64\doxygen.exe Doxygen_config
 
 CALL build\Release\Codewars.exe
