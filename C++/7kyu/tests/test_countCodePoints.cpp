02d0841ac0cb6f62b5eb13d7f0c518c9f4df78e2
Robert Fenyvesi <fenyvesr@gmail.com>
2026-06-04 18:12:01 +0200

    REmove not unicode characters

--- C++/7kyu/tests/test_countCodePoints.cpp
+++ C++/7kyu/tests/test_countCodePoints.cpp
@@ -27,13 +27,9 @@ INSTANTIATE_TEST_CASE_P(
     ::testing::Values(
     // Original/basic examples
     CountCodepointsTestCase{u8"", 0},
-    // A ÿ ♠ 🙉
     CountCodepointsTestCase{"A\xC3\xBF\xE2\x99\xA0\xF0\x9F\x99\x89", 4},
-    // é
     CountCodepointsTestCase{"\xC3\xA9", 1},
-    // e + combining acute accent
     CountCodepointsTestCase{"e\xCC\x81", 2},
-    // 㐷 © ∏ ! 重
     CountCodepointsTestCase{"\xE3\x90\xB7\xC2\xA9\xE2\x88\x8F!\xE9\x87\x8D", 5}
     )
 );
