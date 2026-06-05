d757ba936457e1e901985e78a27377da36336e6c
Robert Fenyvesi <fenyvesr@gmail.com>
2026-06-05 10:22:08 +0200

    Fix Sonar Cloud warnings

--- C++/6kyu/src/restoreBrackets.cpp
+++ C++/6kyu/src/restoreBrackets.cpp
@@ -11,18 +11,20 @@
 **/
 #include "restoreBrackets.h"
 
-std::string restore_brackets(std::string_view s){
-    std::string result{ s };
-    /* The variable 'c' keeps track of the number of child nodes at each depth */
-    short c[256]{};
-    /* The variable 'd' keeps track of the current depth of nested brackets. It is initialized to 0, indicating that we are at the outermost level of the string. */
-    short d{ 0 };
-    for(size_t i{ 0 }; i < result.size(); ++i){
-        /* Close all nested levels, where the number of children is two */
-        for(; (0!= d) && (2 == c[d]); ++c[--d]) result.insert(i++, 1, ']');
-        /* Check for new nested bracket */
-        ('[' == result[i]) ? c[++d] = 0 : ++c[d];
+#include <vector> /* Needed for std::vector */
+
+std::string restore_brackets(std::string_view s) {
+    /* Contains the result */
+    std::string r{ };
+    /* Vector keeping track of the number of children nodes */
+    std::vector<short> b{ 0 };
+    /* Iterates over the input*/
+    for (const auto c : s) {
+        /* Closes all nodes with two children */
+        for (; 2 == b.back(); r += ']', b.pop_back(), ++b.back());
+        c == '[' ? (b.push_back(0), 0) : ++b.back();
+        r+=c;
     }
-    /* Close remaining nested brackets */
-    return result.append(d,']');
+    /* Closes the left over levels */
+    return r.append(b.size() - 1, ']');
 }
