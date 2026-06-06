6b05084ff2b43f4039d3f1239cd9bf5d8e17ab9c
Robert Fenyvesi <fenyvesr@gmail.com>
2026-06-06 18:08:36 +0200

    Try fixing msvc pipeline

--- C++/6kyu/src/allocateRooms.cpp
+++ C++/6kyu/src/allocateRooms.cpp
@@ -11,22 +11,17 @@
 **/
 #include "allocateRooms.h"
 
-#include <range/v3/view/iota.hpp>        /* Needed for ranges::views::iota */
-#include <range/v3/action/sort.hpp>      /* Needed for ranges::actions::sort */
-#include <range/v3/view/transform.hpp>   /* Needed for ranges::views::transform */
-#include <range/v3/range/conversion.hpp> /* Needed for ranges::to */
-
 #include <queue>      /* Needed for std::priority_queue */
 #include <tuple>      /* Needed for std::tuple */
 #include <vector>     /* Needed for std::vector */
 #include <utility>    /* Needed for std::pair */
+#include <algorithm>  /* Needed for std::sort */
 #include <functional> /* Needed for std::greater */
 
 std::vector<int> allocateRooms(const std::vector<std::vector<int>>& customers) {
-    auto mapping = ranges::views::iota(0, static_cast<int>(customers.size()))
-                 | ranges::views::transform([&customers](auto i) { return std::tuple{customers[i][0], customers[i][1], static_cast<int>(i)}; })
-                 | ranges::to<std::vector>()
-                 | ranges::actions::sort;
+    std::vector<std::tuple<int, int, int>> mapping{};
+    for (int i{ 0 }; i < static_cast<int>(customers.size()); mapping.emplace_back(customers[i][0], customers[i][1], i), ++i);
+    std::sort(mapping.begin(), mapping.end());
     std::priority_queue< std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<> > rooms;
     std::vector<int> allocation(customers.size());
     for (auto [arrive, leave, idx] : mapping)
