e6a8ddbf552e961f33ac6a96aa57d74585ca48b1
Robert Fenyvesi <fenyvesr@gmail.com>
2026-06-06 11:44:24 +0200

    Try fixing other workflows.

--- C++/6kyu/src/allocateRooms.cpp
+++ C++/6kyu/src/allocateRooms.cpp
@@ -19,19 +19,20 @@
 #include <queue>      /* Needed for std::priority_queue */
 #include <tuple>      /* Needed for std::tuple */
 #include <vector>     /* Needed for std::vector */
+#include <cstddef>    /* Needed for std::size_t */
 #include <utility>    /* Needed for std::pair */
 #include <functional> /* Needed for std::greater */
 
 std::vector<int> allocateRooms(const std::vector<std::vector<int>>& customers) {
     auto mapping = ranges::views::iota(std::size_t{0}, customers.size())
-                 | ranges::views::transform([&](auto i) { return std::tuple{customers[i][0], customers[i][1], static_cast<int>(i)}; })
+                 | ranges::views::transform([&customers](auto i) { return std::tuple{customers[i][0], customers[i][1], static_cast<int>(i)}; })
                  | ranges::to<std::vector>()
                  | ranges::actions::sort;
     std::priority_queue< std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<> > rooms;
     std::vector<int> allocation(customers.size());
     for (auto [arrive, leave, idx] : mapping)
         (rooms.empty() || rooms.top().first >= arrive)
-            ? rooms.emplace(leave, allocation[idx] = rooms.size() + 1)
-            : (allocation[idx] = rooms.top().second, rooms.pop(), rooms.emplace(leave, allocation[idx]));
+            ? rooms.emplace(leave, allocation[idx] = rooms.size() + 1) // NOSONAR - assignment intended in expression.
+            : (allocation[idx] = rooms.top().second, rooms.pop(), rooms.emplace(leave, allocation[idx])); // NOSONAR - comma operator is intended for performance.
     return allocation;
 }
